eb177e5ced0890c884d41bd2290cd26e7c44287a
Muhammad Maaz <maazafzal92@gmail.com>
2021-04-07 17:06:59 +0500

    Listing Edit Screen with form component

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 import LoginScreen from "./app/screens/LoginScreen";
 
 export default function App() {
-  return <LoginScreen />;
+  return <ListingEditScreen />;
 }
